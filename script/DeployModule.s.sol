34f01a612d30f54da6d9a6c244ea7bd1e50351db
kopy-kat <kopp.konrad@gmail.com>
2024-05-08 14:57:54 +0100

    feat: add spending limit hook example

--- script/DeployModule.s.sol
+++ script/DeployModule.s.sol
@@ -5,13 +5,13 @@ import "forge-std/Script.sol";
 import { RegistryDeployer } from "modulekit/deployment/RegistryDeployer.sol";
 
 // Import modules here
-import { ValidatorTemplate } from "src/ValidatorTemplate.sol";
+import { SpendingLimitHook } from "src/SpendingLimitHook.sol";
 
 /// @title DeployModuleScript
 contract DeployModuleScript is Script, RegistryDeployer {
     function run() public {
         // Setup module bytecode, deploy params, and data
-        bytes memory bytecode = type(ValidatorTemplate).creationCode;
+        bytes memory bytecode = type(SpendingLimitHook).creationCode;
         bytes memory deployParams = "";
         bytes memory data = "";
 
